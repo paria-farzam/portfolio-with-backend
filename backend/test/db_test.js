d4882cb697f58301ed7e342838e320407c3a6596
Paria Farzam <pariafarzam79@gmail.com>
2021-10-30 15:30:42 +0330

    test : add new message

--- backend/test/db_test.js
+++ backend/test/db_test.js
@@ -9,7 +9,7 @@ const about = require("../models/about");
 const admin = require("../models/admin");
 const education = require("../models/education");
 const experience = require("../models/experience");
-const messages = require("../models/messages");
+const message = require("../models/messages");
 
 describe("DB test", () => {
   before(() => {
@@ -24,7 +24,7 @@ describe("DB test", () => {
       .on("error", (error) => {
         console.log(error);
       });
-      done()
+    done();
   });
 
   //test about model
@@ -82,62 +82,82 @@ describe("DB test", () => {
   //test education model
   describe("test education model", () => {
     it("add new education", async () => {
-        let newEducation = new education({
-          year: "2021",
-          title: "education title",
-          desc : "this is the description"
-        });
-        newEducation = await newEducation.save();
-  
-        newEducation.should.be.a("object");
-        newEducation.should.have.property("year");
-        newEducation.should.have.property("title");
-        newEducation.should.have.property("desc");
+      let newEducation = new education({
+        year: "2021",
+        title: "education title",
+        desc: "this is the description",
       });
-  
-      it("find all education", async () => {
-        let newEducation = await education.find({});
-  
-        newEducation.should.be.a("array");
-  
-        for (let i = 0; i < newEducation.length; i++) {
-          newEducation[i].should.be.a("object");
-          newEducation[i].should.have.property("year");
-          newEducation[i].should.have.property("title");
-          newEducation[i].should.have.property("desc");
-        }
-      });
+      newEducation = await newEducation.save();
+
+      newEducation.should.be.a("object");
+      newEducation.should.have.property("year");
+      newEducation.should.have.property("title");
+      newEducation.should.have.property("desc");
+    });
+
+    it("find all education", async () => {
+      let newEducation = await education.find({});
+
+      newEducation.should.be.a("array");
+
+      for (let i = 0; i < newEducation.length; i++) {
+        newEducation[i].should.be.a("object");
+        newEducation[i].should.have.property("year");
+        newEducation[i].should.have.property("title");
+        newEducation[i].should.have.property("desc");
+      }
+    });
   });
 
-  //experience model
+  //test experience model
   describe("test education model", () => {
     it("add new education", async () => {
-        let newExperience = new experience({
-          year: "2021",
-          title: "experience title",
-          desc : "this is the description"
-        });
-        newExperience = await newExperience.save();
-  
-        newExperience.should.be.a("object");
-        newExperience.should.have.property("year");
-        newExperience.should.have.property("title");
-        newExperience.should.have.property("desc");
+      let newExperience = new experience({
+        year: "2021",
+        title: "experience title",
+        desc: "this is the description",
       });
-  
-      it("find all experience", async () => {
-        let newExperience = await experience.find({});
-  
-        newExperience.should.be.a("array");
-  
-        for (let i = 0; i < newExperience.length; i++) {
-          newExperience[i].should.be.a("object");
-          newExperience[i].should.have.property("year");
-          newExperience[i].should.have.property("title");
-          newExperience[i].should.have.property("desc");
-        }
-      });
+      newExperience = await newExperience.save();
+
+      newExperience.should.be.a("object");
+      newExperience.should.have.property("year");
+      newExperience.should.have.property("title");
+      newExperience.should.have.property("desc");
+    });
+
+    it("find all experience", async () => {
+      let newExperience = await experience.find({});
+
+      newExperience.should.be.a("array");
+
+      for (let i = 0; i < newExperience.length; i++) {
+        newExperience[i].should.be.a("object");
+        newExperience[i].should.have.property("year");
+        newExperience[i].should.have.property("title");
+        newExperience[i].should.have.property("desc");
+      }
+    });
   });
+
+  //test messages model
+  describe('test message model', () => {
+      it('send new message', async () => {
+        let newMessage = new message({
+            name : 'tester',
+            email : 'tester@test',
+            subject : 'testing',
+            message : 'this is a test',
+        });
+
+        newMessage = await newMessage.save();
+
+        newMessage.should.be.a('object');
+        newMessage.should.have.property('name')
+        newMessage.should.have.property('email')
+        newMessage.should.have.property('subject')
+        newMessage.should.have.property('message')
+      })
+  })
 
   after(() => {
     mongoose.connection.close();
