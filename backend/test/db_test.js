f56b7ae26ae2aaf7169ce89ba7dd3779c025ea8f
Paria Farzam <pariafarzam79@gmail.com>
2021-10-30 12:21:57 +0330

    test : save about data

--- backend/test/db_test.js
+++ backend/test/db_test.js
@@ -4,6 +4,13 @@ const config = require("config");
 process.env.NODE_ENV = "test";
 const should = require("chai").should();
 
+//import models
+const about = require("../models/about");
+const admin = require("../models/admin");
+const education = require("../models/education");
+const experience = require("../models/experience");
+const messages = require("../models/messages");
+
 describe("DB test", () => {
   before(() => {
     mongoose.Promise = global.Promise;
@@ -12,11 +19,24 @@ describe("DB test", () => {
 
   it("checking the db connection", (done) => {
     mongoose.connection
-      .once("open", () => done())
-      .on("error", (err) => {
-        console.log(err);
-        done();
-      });
+    .once("open", () => done())
+    .on("error", (error) => {console.log(error); done();});
+  });
+
+  describe("chacking about db", () => {
+    it("it should save data in about model", async() => {
+      let aboutData = new about({
+        career: "career",
+        name: "name",
+        desc: "desc",
+      })
+      aboutData = await aboutData.save();
+
+      aboutData.should.be.a('object');
+      aboutData.should.have.property('career');
+      aboutData.should.have.property('name');
+      aboutData.should.have.property('desc');
+    });
   });
 
   after(() => {
