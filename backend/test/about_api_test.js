0ee5f6c51fc509381c11e6b86bb80791c63e9fbc
Paria Farzam <pariafarzam79@gmail.com>
2021-10-31 11:04:28 +0330

    test: /portfolio/about (POST)

--- backend/test/about_api_test.js
+++ backend/test/about_api_test.js
@@ -21,24 +21,33 @@ describe('about api test', () => {
     })
 
     //get method
-    it('GET /portfolio/about', async () => {
+    it('GET /portfolio/about', (done) => {
         chai
         .request(server)
         .get("/portfolio/about")
         .end((err, res)=>{
             should.not.exist(err);
-            res.should.have.property('about');
-            res.about.should.be.a('object');
+            res.should.be.a('object');
             res.about.should.have.property('career');
             res.about.should.have.property('name');
             res.about.should.have.property('desc');
+            done();
         })
     })
     
     //post method
-    // it('POST /portfolio/about', () => {
-
-    // })
+    it('POST /portfolio/about', (done) => {
+        chai
+        .request(server)
+        .post('/portfolio/about')
+        .send({career : 'career', name : 'name', desc : 'desc'})
+        .end((err, res)=>{
+            should.not.exist(err);
+            res.body.should.have.property('msg');
+            res.body.msg.should.be.equal("about added successfully");
+            done();
+        })
+    })
 
     after(()=>mongoose.connection.close())
 })
