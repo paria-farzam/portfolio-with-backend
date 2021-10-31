815f96e24434fca11b03086d8e1e24980d5da2fc
Paria Farzam <pariafarzam79@gmail.com>
2021-10-31 10:24:42 +0330

    test: portfolio/about (GET)

--- backend/test/about_api_test.js
+++ backend/test/about_api_test.js
@@ -1,8 +1,11 @@
 process.env.NODE_ENV='test';
 const mongoose = require('mongoose');
 const config = require('config');
-const about = require('../models/about');
-const should = require('chai').should();
+const chai = require('chai');
+const should = chai.should();
+const server = require('../server');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
 
 describe('about api test', () => {
     before(()=>{
@@ -17,6 +20,25 @@ describe('about api test', () => {
         done();
     })
 
+    //get method
+    it('GET /portfolio/about', async () => {
+        chai
+        .request(server)
+        .get("/portfolio/about")
+        .end((err, res)=>{
+            should.not.exist(err);
+            res.should.have.property('about');
+            res.about.should.be.a('object');
+            res.about.should.have.property('career');
+            res.about.should.have.property('name');
+            res.about.should.have.property('desc');
+        })
+    })
+    
+    //post method
+    // it('POST /portfolio/about', () => {
+
+    // })
 
     after(()=>mongoose.connection.close())
 })
